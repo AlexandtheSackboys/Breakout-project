4393753c2b233971bc28a890c54fe3276fb3f450
AlexandtheSackboys <a.j.costin1@gmail.com>
2025-01-31 14:23:10 +0000

    restructuring code paddle

--- breakout project/Assets/scripts/Paddle_Controller.cs
+++ breakout project/Assets/scripts/Paddle_Controller.cs
@@ -36,25 +36,29 @@ public class Paddle_Controller : MonoBehaviour
             {
                     tilt.SetTrigger("TiltR_Open");
 
-            }
+            } // rotates the paddle in the right direction
+
             else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) {
-                    tilt.SetTrigger("TiltR_Close"); 
-            
-            }
+                    tilt.SetTrigger("TiltR_Close");
+
+            } // returns paddle to original rotation from the right tilt animation
+
+
 
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
                     tilt.SetTrigger("TiltL_Open");
 
-            }
+            } // rotates the paddle in the left direction
+
             else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
             {
                     tilt.SetTrigger("TiltL_Close");
 
-            }
+            } // returns paddle to original rotation from the left tilt animation
 
-            rigidbody.linearVelocity = new Vector3(moveInput.x * speed * Time.deltaTime, 0, 0);
         }
+        rigidbody.linearVelocity = new Vector3(moveInput.x * speed * Time.deltaTime, 0, 0);
     }
 
 
